70a6ad0a00a9a08c865871d5659f6c632f51f973
sunalwaysrise <luwenbin@live.com>
2013-04-24 16:46:34 +0800

    Update slide.js

--- slide.js
+++ slide.js
@@ -104,3 +104,17 @@ var lens=$("#content li").length;
     $("#content li").fadeOut();
     $("#content li").eq(count).fadeIn();
   })
+  
+  
+// 获取URL参数
+var args = {};
+var query = location.search.substring(1);
+var pairs = query.split("&"); 
+for(var i = 0; i < pairs.length; i++) {
+        var pos = pairs[i].indexOf('=');
+        if (pos == -1) continue;
+        var argname = pairs[i].substring(0,pos);
+        var value = pairs[i].substring(pos+1);
+        value = decodeURIComponent(value);
+        args[argname] = value;
+}
